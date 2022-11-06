8e9552b753971c1cad539a517cb6531c06d91296
vohoanglong0107 <mamlong34@gmail.com>
2022-11-06 09:39:05 +0700

    feat: install tailwind

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
   stories: [
     "../components/**/*.stories.mdx",
@@ -7,9 +9,34 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
+    "storybook-addon-next",
   ],
   framework: "@storybook/react",
   core: {
     builder: "@storybook/builder-webpack5",
+  },
+  webpackFinal: (config) => {
+    config.resolve.alias = {
+      ...config.resolve?.alias,
+      "@": path.resolve(__dirname, "../"),
+    };
+    /**
+     * Fixes font import with /
+     * @see https://github.com/storybookjs/storybook/issues/12844#issuecomment-867544160
+     */
+    config.resolve.roots = [
+      path.resolve(__dirname, "../public"),
+      "node_modules",
+    ];
+
+    return config;
   },
 };
