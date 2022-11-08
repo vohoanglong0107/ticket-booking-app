58f6ec3b038f2fa03aefc75cd877c4282d5afaeb
qhuy22601 <qhuy22601@gmail.com>
2022-11-08 16:06:19 +0700

    feat(db): add user profile table

---
+++ prisma/migrations/20221108095643_add_user_pro5_table/migration.sql
@@ -0,0 +1,18 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN', 'STORE_OWNER');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "avatarURL" TEXT,
+    "role" "Role" NOT NULL DEFAULT 'USER',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
