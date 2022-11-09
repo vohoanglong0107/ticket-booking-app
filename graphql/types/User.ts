63023bfa8fce68d6b4d31cb92e4fdb22b281809d
qhuy22601 <qhuy22601@gmail.com>
2022-11-09 18:15:56 +0700

    feat(API): api update user profile

--- graphql/types/User.ts
+++ graphql/types/User.ts
@@ -1,5 +1,12 @@
 import { resolve } from "dns";
-import { enumType, objectType, queryType, stringArg } from "nexus";
+import {
+  enumType,
+  extendType,
+  mutationType,
+  objectType,
+  queryType,
+  stringArg,
+} from "nexus";
 
 export const User = objectType({
   name: "User",
@@ -17,7 +24,8 @@ const Role = enumType({
   members: ["USER", "ADMIN", "STORE_OWNER"],
 });
 
-export const UserQuery = queryType({
+export const UserQuery = extendType({
+  type: "Query",
   definition(t) {
     t.field("user", {
       type: User,
@@ -26,6 +34,33 @@ export const UserQuery = queryType({
       },
       async resolve(_, args, context) {
         return context.prisma.user.findUnique({ where: { id: args.id } });
+      },
+    });
+  },
+});
+
+export const UpdateUser = extendType({
+  type: "Mutation",
+  definition(t) {
+    t.field("user", {
+      type: User,
+      args: {
+        id: stringArg(),
+        name: stringArg(),
+        email: stringArg(),
+        avatarURL: stringArg(),
+        role: Role,
+      },
+      async resolve(_, args, context) {
+        return context.prisma.user.update({
+          where: { id: args.id },
+          data: {
+            name: args.name,
+            email: args.email,
+            avatarURL: args.avatarURL,
+            role: args.role,
+          },
+        });
       },
     });
   },
