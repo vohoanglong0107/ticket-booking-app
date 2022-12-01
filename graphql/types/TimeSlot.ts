fce8e02100202bb8c749c6ece261e148d223b471
vndat00 <ngocdat1908@gmail.com>
2022-12-01 18:15:23 +0700

    feat(db): create query with timeslot model

--- graphql/types/TimeSlot.ts
+++ graphql/types/TimeSlot.ts
@@ -1,4 +1,11 @@
-import { enumType, objectType, queryType, stringArg, extendType } from "nexus";
+import {
+  enumType,
+  objectType,
+  queryType,
+  stringArg,
+  extendType,
+  arg,
+} from "nexus";
 
 export const TimeSlot = objectType({
   name: "TimeSlot",
@@ -7,5 +14,62 @@ export const TimeSlot = objectType({
     t.string("startTime");
     t.string("endTime");
     t.string("game_id");
+  },
+});
+
+export const TimeSlotQuery = extendType({
+  type: "Query",
+  definition(t) {
+    t.nonNull.list.field("timeslots", {
+      type: TimeSlot,
+      async resolve(_parent, _args, context) {
+        return context.prisma.timeSlot.findMany();
+      },
+    });
+  },
+});
+
+export const TimeSlotQueryByGameId = extendType({
+  type: "Query",
+  definition(t) {
+    t.field("timeSlotByGameId", {
+      type: TimeSlot,
+      args: {
+        game_id: stringArg(),
+      },
+      async resolve(parent, args, context) {
+        const timeSlot = await context.prisma.timeSlot.findUnique({
+          where: {
+            id: args.game_id,
+          },
+          include: {
+            game: true,
+          },
+        });
+      },
+    });
+  },
+});
+
+export const TimeSlotCreate = extendType({
+  type: "Mutation",
+  definition(t) {
+    t.field("createTimeSlot", {
+      type: TimeSlot,
+      args: {
+        startTime: stringArg(),
+        endTime: stringArg(),
+        game_id: stringArg(),
+      },
+      async resolve(_, args, context) {
+        return context.prisma.timeSlot.create({
+          data: {
+            startTime: args.startTime,
+            endTime: args.endTime,
+            game_id: args.game_id,
+          },
+        });
+      },
+    });
   },
 });
---
+++ prisma/migrations/20221201105233_alter_time_slot/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "TimeSlot" ALTER COLUMN "startTime" SET DATA TYPE TIME,
+ALTER COLUMN "endTime" SET DATA TYPE TIME;
