2ebb2ed1945ec7d68a555d7151b054be517557be
vohoanglong0107 <mamlong34@gmail.com>
2022-12-08 21:36:33 +0700

    feat: add link to game profile page from index

--- components/View/View-source.tsx
+++ components/View/View-source.tsx
@@ -4,6 +4,7 @@ import ImageListItem from "@mui/material/ImageListItem";
 import ImageListItemBar from "@mui/material/ImageListItemBar";
 import styles from "./View-source.module.css";
 import { itemData } from "@/utils/sample-data";
+import Link from "next/link";
 
 interface Game {
   id: string;
@@ -19,14 +20,18 @@ export default function TitlebarImageList({ games }: Props) {
       <ImageList cols={3}>
         {games.map((game, index) => (
           <ImageListItem key={index} style={{ padding: "30px" }}>
-            <img
-              src={itemData[index % itemData.length].img}
-              style={{ borderRadius: "44px 44px 44px 44px" }}
-            />
-            <ImageListItemBar
-              style={{ textAlign: "center", padding: "20px" }}
-              title={game.title}
-            />
+            <Link href={`/games/${game.id}`}>
+              <a>
+                <img
+                  src={itemData[index % itemData.length].img}
+                  style={{ borderRadius: "44px 44px 44px 44px" }}
+                />
+                <ImageListItemBar
+                  style={{ textAlign: "center", padding: "20px" }}
+                  title={game.title}
+                />
+              </a>
+            </Link>
           </ImageListItem>
         ))}
       </ImageList>
